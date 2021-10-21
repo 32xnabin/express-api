e7dd5cb012e864609c1ed5d612666ad6811ee8ba
nabin.pandey@ebpearls.com <nabin.pandey@ebpearls.com>
2021-10-21 17:57:00 +0545

    feat: apply validate jwt token middleware to case routes

--- app/routes/case.routes.js
+++ app/routes/case.routes.js
@@ -1,28 +1,23 @@
 module.exports = (app) => {
+  const { validateToken } = require('../middlewares/validateToken')
   const cases = require('../controllers/case.controller.js')
 
   var router = require('express').Router()
 
   // Create a new Case
-  router.post('/', cases.create)
+  router.post('/', validateToken, cases.create)
 
   // Retrieve all Cases
-  router.get('/', cases.findAll)
-
-  // Retrieve all published Cases
-  router.get('/published', cases.findAllPublished)
+  router.get('/', validateToken, cases.findAll)
 
   // Retrieve a single Case with id
-  router.get('/:id', cases.findOne)
+  router.get('/:id', validateToken, cases.findOne)
 
   // Update a Case with id
-  router.put('/:id', cases.update)
+  router.put('/:id', validateToken, cases.update)
 
   // Delete a Case with id
-  router.delete('/:id', cases.delete)
-
-  // Create a new Case
-  router.delete('/', cases.deleteAll)
+  router.delete('/:id', validateToken, cases.delete)
 
   app.use('/api/cases', router)
 }
