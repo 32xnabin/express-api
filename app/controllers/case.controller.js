14aede6472f9d014ab84891427b959afbebf4345
nabin <nabin.pandey@ebpearls.com>
2021-11-10 15:24:06 +0545

    feat: add contacts and apartment feilds

--- app/controllers/case.controller.js
+++ app/controllers/case.controller.js
@@ -1,5 +1,5 @@
-const db = require('../models')
-const Myboscase = db.myboscases
+const db = require('../models');
+const Myboscase = db.myboscases;
 
 // Create and Save a new Case
 exports.create = (req, res) => {
@@ -13,6 +13,8 @@ exports.create = (req, res) => {
     category,
     asset_category,
     asset,
+    apartment,
+    contacts,
     assigned_to,
     email_subject,
     email_description,
@@ -21,7 +23,7 @@ exports.create = (req, res) => {
     duplicate_case,
     logged_by,
     images,
-  } = req.body
+  } = req.body;
   // Create a Case
   const myboscase = new Myboscase({
     case_type,
@@ -33,6 +35,8 @@ exports.create = (req, res) => {
     category,
     asset_category,
     asset,
+    apartment,
+    contacts,
     assigned_to,
     email_subject,
     email_description,
@@ -41,97 +45,97 @@ exports.create = (req, res) => {
     duplicate_case,
     logged_by,
     images,
-  })
+  });
 
   // Save Case in the database
   myboscase
     .save(myboscase)
     .then((data) => {
-      res.send(data)
+      res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
         message: err.message || 'Some error occurred while creating the Case.',
-      })
-    })
-}
+      });
+    });
+};
 
 //Retrieve all Cases from the database.
 exports.findAll = (req, res) => {
   Myboscase.find({})
     .sort({ updatedAt: -1 })
     .then((data) => {
-      res.send(data)
+      res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
         message: err.message || 'Some error occurred while retrieving Cases.',
-      })
-    })
-}
+      });
+    });
+};
 
 // Find a single Case with an id
 exports.findOne = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Myboscase.findById(id)
     .then((data) => {
       if (!data)
-        res.status(404).send({ message: 'Not found Case with id ' + id })
-      else res.send(data)
+        res.status(404).send({ message: 'Not found Case with id ' + id });
+      else res.send(data);
     })
     .catch((err) => {
-      res.status(500).send({ message: 'Error retrieving Case with id=' + id })
-    })
-}
+      res.status(500).send({ message: 'Error retrieving Case with id=' + id });
+    });
+};
 
 // Update a Case by the id in the request
 exports.update = (req, res) => {
   if (!req.body) {
     return res.status(400).send({
       message: 'Data to update can not be empty!',
-    })
+    });
   }
 
-  const id = req.params.id
+  const id = req.params.id;
 
   Myboscase.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
     .then((data) => {
       if (!data) {
         res.status(404).send({
           message: `Cannot update Case with id=${id}. Maybe Case was not found!`,
-        })
-      } else res.send({ message: 'Case was updated successfully.' })
+        });
+      } else res.send({ message: 'Case was updated successfully.' });
     })
     .catch((err) => {
       res.status(500).send({
         message: 'Error updating Case with id=' + id,
-      })
-    })
-}
+      });
+    });
+};
 
 // Delete a Case with the specified id in the request
 exports.delete = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Myboscase.findByIdAndRemove(id, { useFindAndModify: false })
     .then((data) => {
       if (!data) {
         res.status(404).send({
           message: `Cannot delete Case with id=${id}. Maybe Case was not found!`,
-        })
+        });
       } else {
         res.send({
           message: 'Case was deleted successfully!',
-        })
+        });
       }
     })
     .catch((err) => {
       res.status(500).send({
         message: 'Could not delete Case with id=' + id,
-      })
-    })
-}
+      });
+    });
+};
 
 // Delete all Cases from the database.
 exports.deleteAll = (req, res) => {
@@ -139,24 +143,24 @@ exports.deleteAll = (req, res) => {
     .then((data) => {
       res.send({
         message: `${data.deletedCount} Cases were deleted successfully!`,
-      })
+      });
     })
     .catch((err) => {
       res.status(500).send({
         message: err.message || 'Some error occurred while removing all Cases.',
-      })
-    })
-}
+      });
+    });
+};
 
 // Find all published Cases
 exports.findAllPublished = (req, res) => {
   Myboscase.find({ published: true })
     .then((data) => {
-      res.send(data)
+      res.send(data);
     })
     .catch((err) => {
       res.status(500).send({
         message: err.message || 'Some error occurred while retrieving Cases.',
-      })
-    })
-}
+      });
+    });
+};
--- app/models/myboscase.model.js
+++ app/models/myboscase.model.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose')
-const AutoIncrement = require('mongoose-sequence')(mongoose)
+const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 module.exports = (mongoose) => {
   var schema = mongoose.Schema(
     {
@@ -13,6 +13,8 @@ module.exports = (mongoose) => {
       category: String,
       asset_category: String,
       asset: String,
+      apartment: String,
+      contacts: String,
       assigned_to: String,
       email_subject: String,
       email_description: String,
@@ -22,15 +24,15 @@ module.exports = (mongoose) => {
       logged_by: String,
       images: [String],
     },
-    { timestamps: true },
-  )
+    { timestamps: true }
+  );
 
   schema.method('toJSON', function () {
-    const { __v, _id, ...object } = this.toObject()
-    object.id = _id
-    return object
-  })
-  schema.plugin(AutoIncrement, { inc_field: 'case_number' })
-  const Myboscase = mongoose.model('myboscase', schema)
-  return Myboscase
-}
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+  schema.plugin(AutoIncrement, { inc_field: 'case_number' });
+  const Myboscase = mongoose.model('myboscase', schema);
+  return Myboscase;
+};
